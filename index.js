937005e50bb57e09dfb78ab5ec6d962b9f8610ce
bengroves <bengroves15@gmail.com>
2021-03-19 14:38:31 -0400

    commit tree 4efa03b5057c5b92111f2dc7d26baf6eed80354c

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(list_of_drivers){ 
+    let bin = []
+    bin.push(list_of_drivers[0])
+    bin.push(list_of_drivers[1])
+    return bin
+}
+
+const returnLastTwoDrivers = function(list_of_drivers){
+    let i = list_of_drivers.length
+    let bin = []
+    bin.push(list_of_drivers[i-2])
+    bin.push(list_of_drivers[i-1])
+    return bin 
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier (multiplier) {
+    return function(fare){
+        return multiplier*fare
+    }
+}
+
+const fareDoubler = function(fare){
+    return fare*2
+}
+
+const fareTripler = function(fare){
+    return fare*3
+}
+
+function selectDifferentDrivers(list_of_drivers, function_called){
+    return function_called(list_of_drivers)
+}
+
